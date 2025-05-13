e8bc680a2e7888130919f48c2669e019a37b3200
Christian Ramet <git@christianramet.com>
2025-05-13 14:50:53 +0200

    style(all): indent tab=2spc

--- lua/cramet/plugins/which-key.lua
+++ lua/cramet/plugins/which-key.lua
@@ -1,13 +1,13 @@
 return{
-	'folke/which-key.nvim',
-	event = 'VimEnter',
-	enable = true,
-	opts = {
-		delay = 200, -- independent of vim.opt.timeoutlen
-		-- Document existing key chains
-		spec = {
-			{ '<leader>s', group = '[S]earch' },
-			{ '<leader>t', group = '[T]oggle' },
-		},
-	},
+  'folke/which-key.nvim',
+  event = 'VimEnter',
+  enable = true,
+  opts = {
+    delay = 200, -- independent of vim.opt.timeoutlen
+    -- Document existing key chains
+    spec = {
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>t', group = '[T]oggle' },
+    },
+  },
 }
