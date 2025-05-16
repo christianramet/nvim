d24828ba4fb48c58a36dbdbd0e9a21ebe66728e4
Christian Ramet <git@christianramet.com>
2025-05-16 09:57:06 +0200

    feat(treesitter): auto_install = true

--- lua/cramet/plugins/treesitter.lua
+++ lua/cramet/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return({
 
     configs.setup({
       ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
